1ff5e7c64d36872a8fbc602c2dbe9a6a7234cac3
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-01-15 01:43:27 +0000

    Added Facebook authentication

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var koa = require('koa')
   , logger = require('koa-logger')
   , bodyParser = require('koa-bodyparser')
+  , session = require('koa-session')
+  , passport = require('koa-passport')
+  , auth = require('./lib/auth')
   , config = require('./lib/config')
   , router = require('./lib/router')
 
 var app = koa()
 
+app.keys = [config.cookiesKey]
+
 app.use(logger())
 app.use(bodyParser())
+app.use(session())
+app.use(passport.initialize())
+app.use(passport.session())
 
+app.use(auth.router.middleware())
+app.use(auth.authenticated)
 app.use(router.middleware())
 
 app.listen(config.port)
